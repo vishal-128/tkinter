637daa0fd3ca9c608604507c16e5613afece19a5
vishal <garg01.vishal@gmail.com>
2021-09-09 20:20:12 +0530

    learn buttons

---
+++ button.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  9 19:43:03 2021
+
+@author: garg0
+"""
+
+from tkinter import *
+root = Tk()
+root.geometry("655x355")
+frame = Frame(root,bg="grey",borderwidth=3,relief =SUNKEN)
+frame.pack(side=LEFT,anchor = "nw")
+
+def hello():
+    print("hello tkinter")
+
+def name():
+    print("vishal garg")
+    
+b1 = Button(frame,fg= "red",text="print", command = hello) # command can be use to call function on clicking the res. button
+b1.pack(side=LEFT,padx=5) #padx gives space between button in x coordinates
+
+b2 = Button(frame,fg= "red",text="print", command=name)
+b2.pack(side=LEFT,padx=5)
+
+b3 = Button(frame,fg= "red",text="print")
+b3.pack(side=LEFT,padx=5)
+
+b4 = Button(frame,fg= "red",text="print")
+b4.pack(side=LEFT,padx=5)
+
+
+
+root.mainloop()
