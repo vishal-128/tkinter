3e6a2e3a7d9de49b0ec35aa4590c8c0e5dea469a
vishal <garg01.vishal@gmail.com>
2021-09-25 19:14:43 +0530

    modified entry page

---
+++ entryPageModified(storing data in dict.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 25 18:58:12 2021
+
+@author: garg0
+"""
+
+from tkinter import *
+root = Tk()
+root.geometry("644x344")
+
+def getvals():
+    print(f"{namevalue.get(),gendervalue.get(),phonevalue.get(),payment_modevalue.get(),emergencyvalue.get(),foodservicevalue.get()}")
+    
+    with open("record.txt","a") as f:  # a stands for append
+        f.write(f"{namevalue.get(),gendervalue.get(),phonevalue.get(),payment_modevalue.get(),emergencyvalue.get(),foodservicevalue.get()}\n")
+    
+    
+    
+    
+Label(root, text="welcome to vishal travells", font='comicsansns 13 bold',pady=15).grid(row=0,column=3)
+name = Label(root,text="name")
+phone = Label(root,text="phone no.")
+gender = Label(root,text="gender")
+payment_mode = Label(root,text="payment mode")
+emergency = Label(root,text="emergency contact")
+
+name.grid(row=1,column=2)
+phone.grid(row=2,column=2)
+gender.grid(row=3,column=2)
+payment_mode.grid(row=4,column=2)
+emergency.grid(row=5,column=2)
+
+namevalue=StringVar()
+gendervalue=StringVar()
+phonevalue=StringVar()
+payment_modevalue=StringVar()
+emergencyvalue=StringVar()
+foodservicevalue=IntVar() #checkbox
+
+nameentry=Entry(root, textvariable=namevalue)
+phoneentry=Entry(root, textvariable=phonevalue)
+genderentry=Entry(root, textvariable=gendervalue)
+paymententry=Entry(root, textvariable=payment_modevalue)
+emergencyentry=Entry(root, textvariable=emergencyvalue)
+
+nameentry.grid(row=1,column=3)
+phoneentry.grid(row=2,column=3)
+genderentry.grid(row=3,column=3)
+paymententry.grid(row=4,column=3)
+emergencyentry.grid(row=5,column=3)
+
+# checkbox
+foodservice=Checkbutton(text="want to prebook the meal",variable=foodservicevalue)
+foodservice.grid(row=6,column=3)
+
+
+Button(text="Submit", command=getvals).grid(row=7,column=3)
+
+
+root.mainloop()
